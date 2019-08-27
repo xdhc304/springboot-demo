e2119b6ee3d0c84fcd3abee6cd0e29f586e0bb5a
jaden.liao <jaden.liao@zoom.us>
2019-08-27 13:32:31 +0800

    update blog controller

--- src/main/java/com/xdhc/demo/web/BlogController.java
+++ src/main/java/com/xdhc/demo/web/BlogController.java
@@ -81,12 +81,12 @@ public class BlogController {
 	 * @return
 	 */
 	@RequestMapping(value = "/getblogbyid", method = RequestMethod.GET)
-	private Map<String, Object> getBlogById(Integer blogId) {
+	private Result<Map<String, Object>> getBlogById(Integer blogId) {
 		Map<String, Object> modelMap = new HashMap<String, Object>();
 		// 获取博客信息
 		Blog blog = blogService.getBlogById(blogId);
 		modelMap.put("blog", blog);
-		return modelMap;
+		return ResultUtil.success(modelMap);
 	}
 
 	/**
